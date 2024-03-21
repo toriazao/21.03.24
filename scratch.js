ad0a1b9bcf70ed8a94a78fbcfda4968efbdb7cbc
toriazao <142436669+toriazao@users.noreply.github.com>
2024-03-21 12:19:38 -0300

    Add files via upload

---
+++ scratch.js
@@ -0,0 +1,141 @@
+const posicaoSuperiorInicial = 70,
+    posicaoEsquerdaInicial = 70,
+    posicaoSuperiorFinal = 500,
+    posicaoEsquerdaFinal = 500,
+    anguloInicial = 0,
+    anguloFinal = 500,
+    imgAvatar = document.getElementById('imgAvatar'),
+    styleAvatar = window.getComputedStyle(imgAvatar),
+    txtPixel = document.getElementById("txtPixel");
+
+var posicaoSuperior = 70,
+    posicaoEsquerda = 70,
+    topAvatar,
+    leftAvatar;
+    anguloAvatar = 0;
+
+imgAvatar.style.position = "absolute";
+imgAvatar.style.top = posicaoSuperior + "px";
+imgAvatar.style.left = posicaoEsquerda + "px";
+imgAvatar.style.transform = "rotate(" + anguloAvatar + "deg)";
+
+function getTopStyle(elementToGet) {
+    let styleElement = window.getComputedStyle(elementToGet);
+    return parseInt(styleElement.getPropertyValue('top').replace("px",''));
+}
+
+function getLeftStyle(elementToGet) {
+    let styleElement = window.getComputedStyle(elementToGet);
+    return parseInt(styleElement.getPropertyValue('left').replace("px",''));
+}
+
+function moverParaEsquerda() {
+    console.clear();
+    console.log("Posição do avatar à esquerda antes: ", imgAvatar.style.left);
+    leftAvatar = getLeftStyle(imgAvatar);
+    if (leftAvatar > posicaoEsquerdaInicial) {
+        posicaoEsquerda -= parseInt(txtPixel.value);
+        if (posicaoEsquerda < posicaoEsquerdaInicial) {
+            posicaoEsquerda = posicaoEsquerdaInicial;
+        }
+        imgAvatar.style.left = posicaoEsquerda + "px";
+    } else {
+        console.error("Ops! Chegou ao limite e não é possível mais mover para esquerda. Tente outro botão.");
+    }
+    console.log("Posição do avatar à esquerda depois: ", imgAvatar.style.left);
+}
+
+function moverParaDireita() {
+    console.clear();
+    console.log("Posição do avatar à esquerda antes: ", imgAvatar.style.left);
+    leftAvatar = getLeftStyle(imgAvatar);
+    if (leftAvatar < posicaoEsquerdaFinal) {
+        posicaoEsquerda += parseInt(txtPixel.value);
+        if (posicaoEsquerda > posicaoEsquerda) {
+            posicaoEsquerda = posicaoEsquerdaFinal;
+        }
+        imgAvatar.style.left = posicaoEsquerda + "px";
+    } else {
+        console.error("Ops! Chegou ao limite e não é possível mais mover para direita. Tente outro botão.");
+    }
+    console.log("Posição do avatar à esquerda depois: ", imgAvatar.style.left);
+}
+
+function moverParaCima() {
+    console.clear();
+    console.log("Posição superior do avatar antes: ", imgAvatar.style.top);
+    topAvatar = getTopStyle(imgAvatar);
+    if (topAvatar > posicaoSuperiorInicial) {
+        posicaoSuperior -= parseInt(txtPixel.value);
+        if (posicaoSuperior < posicaoSuperiorInicial) {
+            posicaoSuperior = posicaoSuperiorInicial;
+        }
+        imgAvatar.style.top = posicaoSuperior + "px";
+    } else {
+        console.error("Ops! Chegou ao limite e não é possível mais mover para cima. Tente outro botão.");
+    }
+    console.log("Posição superior do avatar depois: ", imgAvatar.style.top);
+}
+
+function moverParaBaixo() {
+    console.clear();
+    console.log("Posição superior do avatar antes: ", imgAvatar.style.top);
+    topAvatar = getTopStyle(imgAvatar);
+    if (topAvatar < posicaoSuperiorFinal) {
+        posicaoSuperior += parseInt(txtPixel.value);
+        if (posicaoSuperior > posicaoSuperiorFinal) {
+            posicaoSuperior = posicaoSuperiorFinal;
+        }
+        imgAvatar.style.top = posicaoSuperior + "px";
+    } else {
+        console.error("Ops! Chegou ao limite e não é possível mais mover para cima. Tente outro botão.");
+    }
+    console.log("Posição superior do avatar depois: ", imgAvatar.style.top);
+}
+
+function girarHorario() {
+    console.clear();
+    console.log("Ângulo do avatar antes: ", anguloAvatar + "deg");
+    if (anguloAvatar < anguloFinal) {
+        anguloAvatar += parseInt(txtPixel.value);
+        if (anguloAvatar > anguloFinal) {
+            anguloAvatar = anguloFinal;
+        }
+        imgAvatar.style.transform = "rotate(" + anguloAvatar +"deg)";
+    } else {
+        console.error("Ops! Chegou ao limite e não é possível mais girar no sentido horário. Tente outro botão.");
+    }
+    console.log("ângulo do avatar depois: ", anguloAvatar + "deg");
+}
+
+function girarAntiHorario() {
+    console.clear();
+    console.log("Ângulo do avatar antes: ", anguloAvatar + "deg");
+    if (anguloAvatar > anguloInicial) {
+        anguloAvatar -= parseInt(txtPixel.value);
+        if (anguloAvatar < anguloInicial) {
+        anguloAvatar = anguloInicial; 
+        } 
+        imgAvatar.style.transform = "rotate(" + anguloAvatar + "deg)";
+    } else {
+        console.error("Ops! Chegou ao limite e não é possível mais girar no sentido horário. Tente outro botão.");
+    }
+    console.log("Ângulo do avatar depois: ", anguloAvatar + "deg");
+}
+
+function deslizarParaDireita() {
+    var img = document.getElementById('imgAvatar');
+    var posicaoInicial = 0;
+    var posicaoFinal = 300;
+    var velocidade = 5;
+    var id = setInterval(frame, 5);
+
+    function frame() {
+        if (posicaoInicial == posicaoFinal) {
+            clearInterval(id);
+        } else {
+          posicaoInicial++;
+          img.style.left = posicaoInicial + 'px';
+        }
+    }
+}
